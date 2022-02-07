bed1b10969a77f872df488108bd47fa3f40c6a26
Emerson SIlva <94561911+Emerson4002@users.noreply.github.com>
2022-02-07 00:03:53 -0300

    finalizado
    Fim do curso de JavaScript do CursoEmVIdeo

---
+++ exercicios/ex017/script.js
@@ -0,0 +1,59 @@
+var num = document.querySelector("input#txtn")
+var lista = document.querySelector("select#flist")
+var res = document.querySelector("div#res")
+var valores = []
+
+function isNumero(n){
+    if(Number(n) >=1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
+}
+
+function adiciona(){
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `esse valor ${num.value} foi adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ""
+    }else{
+        window.alert("Numero invalido ou ja está adicionado")
+    }
+        num.value = ""
+        num.focus()
+}
+function finalizar(){
+    if(valores.length == 0 ){
+        window.alert("Por favor insira um numero antes de finalizar")
+    }else{
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior){
+                maior = valores[pos]}
+            if(valores[pos] < menor){
+                menor = valores[pos]
+            }
+        }
+        media = soma/tot
+        res.innerHTML = ""
+        res.innerHTML += `<p>ao todo temos ${tot} elementos cadastrados</p>`
+        res.innerHTML += `<p>o maior valor informado foi ${maior} </p>` 
+        res.innerHTML += `<p>o menor valor informado foi ${menor} </p>`
+        res.innerHTML += `<p>a soma dos valores é ${soma}</p>`
+        res.innerHTML += `<p>a media dos valores é ${media}</p>`
+    }
+}
